fb3d6d62d5d0967f3871a07e6ed279331257d45f
Firmansyah Dwi Nanda <firmansyahdwinanda@gmail.com>
2021-10-08 08:28:29 +0700

    upload file

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+$(function() {
+    $("#playlist li").on("click", function() {
+        $("#videoarea").attr({
+            "src": $(this).attr("movieurl"),
+            "poster": "",
+            "autoplay": false
+        })
+    })
+    $("#videoarea").attr({
+        "src": $("#playlist li").eq(0).attr("movieurl"),
+        "poster": $("#playlist li").eq(0).attr("moviesposter")
+    })
+})
